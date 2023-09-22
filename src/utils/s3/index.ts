640b8cdc5b9178ad98ac16c403eb64d0ef808259
Philip Andersson <philipodev@gmail.com>
2023-09-22 09:22:42 +0200

    update

--- src/utils/s3/index.ts
+++ src/utils/s3/index.ts
@@ -7,6 +7,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const client = new S3Client({
   endpoint: 'https://fra1.digitaloceanspaces.com',
+  region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
